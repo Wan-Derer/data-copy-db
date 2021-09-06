00caa2a597d9be0eb7695d696f275515ce97b2db
Wan-Derer <frd-ru@yandex.ru>
2021-09-06 13:12:25 +0300

    Completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,104 +11,71 @@ Insert для второй БД например insert into second_scheme.table
  */
 
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        final Connection connection1 = DriverManager.getConnection
-                ("jdbc:h2:./base1", "user1", "password1");
+  public static void main(String[] args) throws SQLException {
+    final Connection connection1 = DriverManager.getConnection
+      ("jdbc:h2:./base1", "user1", "password1");
 
-        final Connection connection2 = DriverManager.getConnection
-                ("jdbc:h2:./base2", "user2", "password2");
+    final Connection connection2 = DriverManager.getConnection
+      ("jdbc:h2:./base2", "user2", "password2");
 
-        dataCopy(connection1,
-                connection2,
-                "select name, address from sch.clients1",
-                "insert into second_scheme.table2(name2,addr2)");
+    dataCopy(connection1,
+      connection2,
+      "select name,address from sch1.clients1",
+      "insert into sch2.clients2(name2,addr2)");
 
 
-//        String selectQuery = "SELECT * FROM public.clients1";
-//        PreparedStatement statement = connection1.prepareStatement(selectQuery);
-//        boolean hasResult = statement.execute();
-//
-//        ResultSet resultSet = statement.getResultSet();
-//
-//        while (resultSet.next()) {
-//            System.out.println(resultSet.getString("name") + "\t" + resultSet.getString("address"));
-////            System.out.println(resultSet.getObject("int_field"));
-//        }
-//
-//        DatabaseMetaData metaData = connection1.getMetaData();
-//        resultSet = metaData.getTables("BASE1", "PUBLIC", "%", null);
-////        while (resultSet.next()){
-//            resultSet.next();
-//            System.out.println(resultSet.getRowId(2));
-//            System.out.println(resultSet.getString(1));
-//            System.out.println(resultSet.getString(2));
-//            System.out.println(resultSet.getString(3));
-//            System.out.println(resultSet.getString(4));
-//            System.out.println(resultSet.getString(5));
-//            System.out.println(resultSet.getString(7));
-////        }
-//
-////        System.out.println(statement.execute());
-//
+  }
 
-    }
+  private static void dataCopy(Connection conn1, Connection conn2, String selectQuery, String insertQuery) {
 
-    private static void dataCopy(Connection conn1, Connection conn2, String selectQuery, String insertQuery) {
-        // получаем поля, которые надо скопировать
+    // получаем поля, которые надо скопировать
+    // парсинг Select
+    String[] selectFields = Arrays.stream(selectQuery.toLowerCase().split("[ ,()]"))
+      .dropWhile(s -> !"select".equals(s))
+      .skip(1)
+      .takeWhile(s -> !"from".equals(s))
+      .filter(s -> !s.isEmpty())
+      .toArray(String[]::new);
 
-        // парсинг Select
-        String selectFieldsStr = selectQuery.toLowerCase();
-        int start = selectFieldsStr.indexOf("select") + "select".length();
-        int stop = selectFieldsStr.indexOf("from");
-        selectFieldsStr = selectFieldsStr.substring(start, stop);
+    try (PreparedStatement selStatement = conn1.prepareStatement(selectQuery)) {
 
-        String[] selectFields = Arrays.stream(selectFieldsStr.split(","))
-                .map(String::strip)
-                .toArray(String[]::new);
+      selStatement.execute();
+      ResultSet resultSet = selStatement.getResultSet();
 
-        // парсинг Insert
-        String insertFieldsStr = insertQuery.toLowerCase();
-        start = insertFieldsStr.indexOf("into") + 4;
-        insertFieldsStr = insertQuery.substring(start);
-        start = insertFieldsStr.indexOf('(') + 1;
-        insertFieldsStr = insertFieldsStr.substring(start);
-        insertFieldsStr = insertFieldsStr.replaceAll("[)]", "");
+      // при выборке из БД любое поле может быть представлено
+      // как String или Object независимо от его реального типа
+      StringBuilder insQueryBuilder;
 
-        String[] insertFields = Arrays.stream(insertFieldsStr.split(","))
-                .map(String::strip)
-                .toArray(String[]::new);
+      while (resultSet.next()) {
+        insQueryBuilder = new StringBuilder(insertQuery);
+        insQueryBuilder.setLength(insQueryBuilder.lastIndexOf(")"));
+        insQueryBuilder.append(", updated_at)\n values ('");    // добавляем дату/время вставки данных
 
-        // если кол-во полей для выборки и вставки не совпало - ошибка
-        if (selectFields.length != insertFields.length){
-            throw new IllegalArgumentException("Select and Insert field qty must be the same");
+        for (String selectField : selectFields) {
+          insQueryBuilder.append(resultSet.getString(selectField)).append("', '");
         }
 
-        try(
-                PreparedStatement selStatement = conn1.prepareStatement(selectQuery);
-                PreparedStatement insStatement = conn2.prepareStatement(insertQuery);
-        ) {
+        insQueryBuilder.append(LocalDateTime.now()).append("');\n");
 
-            boolean hasResult = selStatement.execute();
-            ResultSet resultSet = selStatement.getResultSet();
+        PreparedStatement insStatement = conn2.prepareStatement(insQueryBuilder.toString());
+        insStatement.execute();
+        insStatement.close();
 
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("name") + "\t" + resultSet.getString("address"));
-//            System.out.println(resultSet.getObject("int_field"));
-            }
-        }catch (SQLException e){
+//        System.out.println(insQueryBuilder);
+      }
 
-        }
+    } catch (SQLException e) {
+      // здесь могут возникнуть ошибки несоответствия типов полей выборки и вставки, синтаксические ошибки в
+      // пришедших строках-запросах, просто ошибки SQL. Вопрос - что с ними делать (помимо логирования). Можно
+      // сформировать своё исключение и или "пробросить" возникшее, или обработать как-то ещё
 
-
-
-        System.out.println(Arrays.toString(insertFields));
-
-
+      e.printStackTrace();
     }
+
+
+  }
 }
